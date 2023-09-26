316af7aace15510a52e3779e7129c72284698805
Iliyas Ukenov <iliyasuken@gmail.com>
2023-09-26 23:12:48 +0600

    practise 4

---
+++ Lesson4/ConsoleApp.HomeWork4/ConsoleApp.HomeWork4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryHomeWork4;
+
+namespace ConsoleApp.HomeWork4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Airplane airplane = new Airplane();
+            string newModel = "Новая модель";
+            double newWeight = 10000.0;
+            int newCountOfSeats = 150;
+
+            airplane.UpdateAirplane(ref newModel, ref newWeight, ref newCountOfSeats);
+        }
+    }
+}
