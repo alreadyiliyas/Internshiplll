a7e27ccda37f9220750beb4729ecf11bc080a5a5
Iliyas Ukenov <iliyasuken@gmail.com>
2023-11-19 14:26:42 +0600

    Lesson 11

--- Lesson11/ConsoleApp.Homework11/ConsoleApp.Homework11/Program.cs
+++ Lesson11/ConsoleApp.Homework11/ConsoleApp.Homework11/Program.cs
@@ -17,7 +17,32 @@ namespace ConsoleApp.Homework11
             int choise = Convert.ToInt32(Console.ReadLine());
             if (choise == 1)
             {
-                
+                List<Employee> employees = new List<Employee>();
+                Console.WriteLine("Кол-во работников: ");
+                int countEmp = Convert.ToInt32(Console.ReadLine());
+
+                if (countEmp > 0)
+                {
+                    for (int i = 0; i < countEmp; i++)
+                    {
+                        IEmployee employee = new Employee();
+                        Console.WriteLine("Имя сотрудника: ");
+                        employee.Name = Console.ReadLine();
+                        Console.WriteLine("Позиция сотрудника: ");
+                        employee.Position = Console.ReadLine();
+                        Console.WriteLine("Зарплата сотрудника: ");
+                        employee.Salary = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите дату (в формате YYYY-MM-DD):");
+                        string inputDate = Console.ReadLine();
+                        if (DateTime.TryParse(inputDate, out DateTime hireDate)) { employee.HireDate = hireDate; }
+                        else { Console.WriteLine("Неверный формат даты. Пожалуйста, введите дату в формате YYYY-MM-DD."); }
+                        Console.WriteLine("Введите пол сотрудника: ");
+                        employee.Gender = Console.ReadLine();
+                        employees.Add((Employee)employee);
+                    }
+                    Ex1(choise, employees);
+                }
+                else { Console.WriteLine("Ошибка выбран не правильный параметр"); }
             }
             else if (choise == 2)
             {
@@ -55,109 +80,7 @@ namespace ConsoleApp.Homework11
                     employee.Gender = "F";
                     employees.Add((Employee)employee);
                 }
-                Console.WriteLine("вывести полную информацию обо всех сотрудниках: 3");
-                Console.WriteLine("вывести полную информацию о сотрудниках, выбранной должности: 4");
-                Console.WriteLine("найти в массиве всех менеджеров, зарплата которых больше средней зарплаты всех клерков, " +
-                    "\nвывести на экран полную информацию о таких менеджерах отсортированной по их фамилии: 5");
-                Console.WriteLine("распечатать информацию обо всех сотрудниках, принятых на работу " +
-                    "\nпозже определенной даты (дата передается пользователем), " +
-                    "\nотсортированную в алфавитном порядке по фамилии сотрудника.: 6");
-                Console.WriteLine("Вывести информацию о всех мужчинах, женщинах в зависимости от того что передаст пользователь. " +
-                    "\nПредусмотреть случай, когда, пользователь не выбирает конкретный пол, т.е. просто хочет получить информацию о всех.: 7");
-                choise = Convert.ToInt32(Console.ReadLine());
-                //All
-                if (choise == 3 ) 
-                {
-                    foreach (var employee in employees)
-                    {
-                        Console.WriteLine(employee);
-                        Console.WriteLine();
-                    };
-                }
-                //Position
-                else if (choise == 4 )
-                {
-                    Console.WriteLine("Введите должность: ");
-                    string Pos = Console.ReadLine();
-                    var PosEmployees = employees.FindAll(x => x.Position == Pos);
-                    foreach (var PosEmployee in PosEmployees)
-                    {
-                        Console.WriteLine(PosEmployee);
-                    }
-                }
-                //Avg
-                else if (choise == 5)
-                {
-                    string Pos = "Manager";
-                    var PosEmployees = employees.FindAll(x => x.Position == Pos);
-                    var AvgSalary = employees.Where(x => x.Position == "Manager").Average(x => x.Salary);
-                    Console.WriteLine("Средняя зарплата: ");
-                    Console.WriteLine("---------------------------------------");
-                    Console.WriteLine(AvgSalary);
-                    Console.WriteLine("---------------------------------------");
-                    Console.WriteLine("Сотрудники у которых больше средней: ");
-
-                    var EmpAvgSalaryMore = employees.Where(x => x.Position == "Manager" && x.Salary > AvgSalary);
-
-                    foreach (var more in EmpAvgSalaryMore)
-                    {
-                        Console.WriteLine(more);
-                    }
-                }
-                else if (choise == 6)
-                {
-                    DateTime HiredDateEmp;
-                    foreach (var employee in employees)
-                    {
-                        Console.WriteLine(employee);
-                        Console.WriteLine();
-                    };
-                    Console.WriteLine("Введите дату (в формате YYYY-MM-DD):");
-                    if (DateTime.TryParse(Console.ReadLine(), out HiredDateEmp))
-                    {
-                        var HiredDateMore = employees.FindAll(x => x.HireDate > HiredDateEmp).OrderBy(x => x.Name);
-
-                        Console.WriteLine("------------------");
-                        foreach (var dateMore in HiredDateMore)
-                        {
-                            Console.WriteLine(dateMore);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Некорректный формат даты!");
-                    }
-
-                }
-                else if (choise == 7)
-                {
-                    Console.WriteLine("Введите пол (F-женский, M-мужской): ");
-                    string genderInput = Console.ReadLine();
-                    if (genderInput == "F")
-                    {
-                        var genderF = employees.FindAll(x => x.Gender == genderInput);
-                        foreach (var gender in genderF)
-                        {
-                            Console.WriteLine(gender);
-                        }
-                    }
-                    else if (genderInput == "M") 
-                    {
-                        var genderF = employees.FindAll(x => x.Gender == genderInput);
-                        foreach (var gender in genderF)
-                        {
-                            Console.WriteLine(gender);
-                        }
-                    }
-                    else
-                    {
-                        foreach (var employee in employees)
-                        {
-                            Console.WriteLine(employee);
-                            Console.WriteLine();
-                        };
-                    }
-                }
+                Ex1(choise, employees);
             }
             else { Console.WriteLine("Ошибка выбран не правильный параметр"); }
         }
@@ -167,6 +90,112 @@ namespace ConsoleApp.Homework11
             int month = random.Next(1, 13); // месяц от 1 до 12
             int day = random.Next(1, DateTime.DaysInMonth(year, month) + 1); // день от 1 до максимального числа дней в месяце
             return new DateTime(year, month, day);
+        }
+        static void Ex1(int choise, List<Employee> employees)
+        {
+            Console.WriteLine("вывести полную информацию обо всех сотрудниках: 3");
+            Console.WriteLine("вывести полную информацию о сотрудниках, выбранной должности: 4");
+            Console.WriteLine("найти в массиве всех менеджеров, зарплата которых больше средней зарплаты всех клерков, " +
+                "\nвывести на экран полную информацию о таких менеджерах отсортированной по их фамилии: 5");
+            Console.WriteLine("распечатать информацию обо всех сотрудниках, принятых на работу " +
+                "\nпозже определенной даты (дата передается пользователем), " +
+                "\nотсортированную в алфавитном порядке по фамилии сотрудника.: 6");
+            Console.WriteLine("Вывести информацию о всех мужчинах, женщинах в зависимости от того что передаст пользователь. " +
+                "\nПредусмотреть случай, когда, пользователь не выбирает конкретный пол, т.е. просто хочет получить информацию о всех.: 7");
+            choise = Convert.ToInt32(Console.ReadLine());
+            //All
+            if (choise == 3)
+            {
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine(employee);
+                    Console.WriteLine();
+                };
+            }
+            //Position
+            else if (choise == 4)
+            {
+                Console.WriteLine("Введите должность: ");
+                string Pos = Console.ReadLine();
+                var PosEmployees = employees.FindAll(x => x.Position == Pos);
+                foreach (var PosEmployee in PosEmployees)
+                {
+                    Console.WriteLine(PosEmployee);
+                }
+            }
+            //Avg
+            else if (choise == 5)
+            {
+                string Pos = "Manager";
+                var PosEmployees = employees.FindAll(x => x.Position == Pos);
+                var AvgSalary = employees.Where(x => x.Position == "Manager").Average(x => x.Salary);
+                Console.WriteLine("Средняя зарплата: ");
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine(AvgSalary);
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("Сотрудники у которых больше средней: ");
+
+                var EmpAvgSalaryMore = employees.Where(x => x.Position == "Manager" && x.Salary > AvgSalary);
+
+                foreach (var more in EmpAvgSalaryMore)
+                {
+                    Console.WriteLine(more);
+                }
+            }
+            else if (choise == 6)
+            {
+                DateTime HiredDateEmp;
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine(employee);
+                    Console.WriteLine();
+                };
+                Console.WriteLine("Введите дату (в формате YYYY-MM-DD):");
+                if (DateTime.TryParse(Console.ReadLine(), out HiredDateEmp))
+                {
+                    var HiredDateMore = employees.FindAll(x => x.HireDate > HiredDateEmp).OrderBy(x => x.Name);
+
+                    Console.WriteLine("------------------");
+                    foreach (var dateMore in HiredDateMore)
+                    {
+                        Console.WriteLine(dateMore);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный формат даты!");
+                }
+
+            }
+            else if (choise == 7)
+            {
+                Console.WriteLine("Введите пол (F-женский, M-мужской): ");
+                string genderInput = Console.ReadLine();
+                if (genderInput == "F")
+                {
+                    var genderF = employees.FindAll(x => x.Gender == genderInput);
+                    foreach (var gender in genderF)
+                    {
+                        Console.WriteLine(gender);
+                    }
+                }
+                else if (genderInput == "M")
+                {
+                    var genderF = employees.FindAll(x => x.Gender == genderInput);
+                    foreach (var gender in genderF)
+                    {
+                        Console.WriteLine(gender);
+                    }
+                }
+                else
+                {
+                    foreach (var employee in employees)
+                    {
+                        Console.WriteLine(employee);
+                        Console.WriteLine();
+                    };
+                }
+            }
         }
     }
 }
